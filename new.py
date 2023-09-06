f9b715fd2960a6e57151c63ed89324b8b4911907
SamGuvi <116014778+SamGuvi@users.noreply.github.com>
2023-09-06 22:28:36 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,28 @@
+from newspaper import Article
+import streamlit as st
+import openai
+
+def Summarize(news_data):
+    openai.api_key = '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'
+    summarize = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=f'Summarize in 30 words "{news_data}"',
+        max_tokens=1000,
+        temperature=0.7,
+    )
+    return summarize.choices[0].text
+
+Url=st.text_input('Enter the URL')
+if st.button('Summarize'):
+    news_data = Article(Url)
+    news_data.download()
+    news_data.parse()
+    title=news_data.title
+    summary=news_data.text
+    st.title(title)
+    st.image(news_data.top_image, width=500)
+    s="".join(summary).replace('\n','')
+    summarized=Summarize(s)
+    st.write(summarized)
+    
+
